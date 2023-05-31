44fdeaeff763f15a1a5716d730e51122574a31f1
dpolimeni <diego.polimeni99@gmail.com>
2023-05-31 18:39:26 +0200

    Adjusted profile settings page with some widgets and other profile fields

--- users/forms.py
+++ users/forms.py
@@ -2,28 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import CustomUser, playerProfile, clubProfile
-from django.forms import TextInput, NumberInput, ChoiceField
+from django.forms import TextInput, NumberInput, ChoiceField, EmailInput
 
-class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'which_account']
-        
-class UserUpdateForm(forms.ModelForm):
-    email = forms.EmailField(required=True)
-    
-    class Meta:
-        model = CustomUser
-        fields = ['username', 'email']
-
-class ProfileSignUpForm(forms.ModelForm):
-    class Meta:
-        model = playerProfile
-        fields = ['name', 'surname']
-        
-        widgets = {
+widgets = {
             'name': TextInput(attrs={
                 'class': "form-control mb-3",
                 #'style': 'max-width: 300px;',
@@ -33,19 +14,6 @@ class ProfileSignUpForm(forms.ModelForm):
                 'class': "form-control mb-3",
                 #'style': 'max-width: 300px;',
                 'placeholder': 'Cognome'
-                })
-        }
-        
-class ClubSignUpForm(forms.ModelForm):
-    class Meta:
-        model = clubProfile
-        fields = ['name', 'city', 'number_holes']
-        
-        widgets = {
-            'name': TextInput(attrs={
-                'class': "form-control mb-3",
-                #'style': 'max-width: 300px;',
-                'placeholder': 'Nome'
                 }),
             'city': TextInput(attrs={
                 'class': "form-control mb-3",
@@ -54,13 +22,59 @@ class ClubSignUpForm(forms.ModelForm):
                 }),
             'number_holes': forms.Select(attrs={
                 'class': "select form-select",
+                }),
+            'experience': forms.Select(attrs={
+                'class': "select form-select",
+                }),
+            'username': TextInput(attrs={
+                'class': "form-control mb-3",
+                #'style': 'max-width: 300px;',
+                'placeholder': 'username'
+                }),
+            'email': EmailInput(attrs={
+                'class': "emailinput form-control mb-3",
+                #'style': 'max-width: 300px;',
+                'placeholder': 'email',
+                'required': True
                 })
         }
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password1', 'password2', 'which_account']
+        widgets = widgets
         
-class ProfileUpdateForm(forms.ModelForm):
+class UserUpdateForm(forms.ModelForm):
+    #email = forms.EmailField(required=True, widget=widgets)
+    
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email']
+        widgets = widgets
+
+class ProfileSignUpForm(forms.ModelForm):
     class Meta:
         model = playerProfile
-        fields = ['img']
+        fields = ['name', 'surname']
+        widgets = widgets
+
+        
+class ClubSignUpForm(forms.ModelForm):
+    class Meta:
+        model = clubProfile
+        fields = ['name', 'city', 'number_holes']
+        widgets = widgets
+
+        
+class ProfileUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = playerProfile
+        fields = ['name', 'surname', 'experience', 'img']
+        widgets = widgets
         
 class ClubUpdateForm(forms.ModelForm):
     class Meta:
