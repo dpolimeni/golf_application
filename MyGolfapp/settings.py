55bc1b28c4208b77961d91dd97e45c4bd8202a4f
dpolimeni <diego.polimeni99@gmail.com>
2023-06-23 15:33:00 +0200

    No message 404

--- MyGolfapp/settings.py
+++ MyGolfapp/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0lny*!2tx6wepd2rcf+^y12+t)-fa)jt@##n7acu))rtu*crj!'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -155,5 +156,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'diego.polimeni@gmail.com' ## os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = 'oulgstyntvbgyenw' ## os.environ.get('EMAIL_PASS')
+EMAIL_HOST_USER =os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
