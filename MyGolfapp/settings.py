71da0c0dc34b38b201e3692f64a4b8df161cb775
dpolimeni <diego.polimeni99@gmail.com>
2023-10-16 09:03:11 +0200

    update settings

--- MyGolfapp/settings.py
+++ MyGolfapp/settings.py
@@ -32,10 +32,12 @@ ALLOWED_HOSTS = ["*"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://golfapp-mike.azurewebsites.net",
+    "https://testgolf-mike-2.azurewebsites.net"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://golfapp-mike.azurewebsites.net",
+    "https://testgolf-mike-2.azurewebsites.net"
 ]
 
 # Application definition
